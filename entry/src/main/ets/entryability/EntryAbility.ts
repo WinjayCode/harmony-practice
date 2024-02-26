3ad3cea3f8d1b67fdd8a09e938f956ef4cc4d47b
Winjay <wen-jie.duan@mobiledrivetech.com>
2024-02-26 16:53:12 +0800

    page/ability jump

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -23,14 +23,6 @@ export default class EntryAbility extends UIAbility {
       }
       hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
     });
-
-    // windowStage.loadContent('pages/Hello', (err, data) => {
-    //   if (err.code) {
-    //     hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
-    //     return;
-    //   }
-    //   hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
-    // });
   }
 
   onWindowStageDestroy() {
